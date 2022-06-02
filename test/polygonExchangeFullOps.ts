6ea60638a0c8ac0b17396a6e6cb1a32292106507
toolback <julien.rnlt@gmail.com>
2022-06-02 20:23:46 +0200

    test

--- test/polygonExchangeFullOps.ts
+++ test/polygonExchangeFullOps.ts
@@ -17,15 +17,16 @@ describe("Exchange (full setup operations)", async () => {
     let exchange: Exchange;
     let eurt: IERC20Metadata, dai: IERC20Metadata, usdc: IERC20Metadata,
         usdt: IERC20Metadata, weth: IWrappedEther, eurtLp: IERC20Metadata,
-        alluo: IERC20Metadata;
+        alluo: IERC20Metadata, wMatic: IWrappedEther;
 
     let eurtDaiRoute: Route, eurtUsdcRoute: Route, eurtUsdtRoute: Route, daiEurtRoute: Route, daiUsdcRoute: Route, daiUsdtRoute: Route,
-        usdcEurtRoute: Route, usdcDaiRoute: Route, usdcUsdtRoute: Route, usdtEurtRoute: Route, usdtDaiRoute: Route, usdtUsdcRoute: Route;
+        usdcEurtRoute: Route, usdcDaiRoute: Route, usdcUsdtRoute: Route, usdtEurtRoute: Route, usdtDaiRoute: Route, usdtUsdcRoute: Route,
+        wMaticEurtRoute: Route, wMaticDaiRoute: Route, wMaticUsdtRoute: Route, wMaticUsdcRoute: Route;
     // let wethUsdcRoute: Route, wethDaiRoute: Route, usdtWethRoute: Route;
 
     let eurtEdge: Edge;
 
-    const eurtPoolAddress = "0x9b3d675FDbe6a0935E8B7d1941bc6f78253549B7";
+    const eurtPoolAddress = "0x225fb4176f0e20cdb66b4a3df70ca3063281e855";
     const uint256MaxValue = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
     const nativeEth = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
     const zeroAddr = "0x0000000000000000000000000000000000000000";
@@ -88,6 +89,23 @@ describe("Exchange (full setup operations)", async () => {
             { swapProtocol: 2, pool: eurtPoolAddress, fromCoin: usdt.address, toCoin: usdc.address }
         ];
 
+        wMaticEurtRoute = [
+            // USDT - WETH
+            { swapProtocol: 2, pool: eurtPoolAddress, fromCoin: wMatic.address, toCoin: eurt.address }
+        ];
+        wMaticDaiRoute = [
+            // USDT - WETH
+            { swapProtocol: 2, pool: eurtPoolAddress, fromCoin: wMatic.address, toCoin: dai.address }
+        ];
+        wMaticUsdcRoute = [
+            // USDT - WETH
+            { swapProtocol: 2, pool: eurtPoolAddress, fromCoin: wMatic.address, toCoin: usdc.address }
+        ];
+        wMaticUsdtRoute = [
+            // USDT - WETH
+            { swapProtocol: 2, pool: eurtPoolAddress, fromCoin: wMatic.address, toCoin: usdt.address }
+        ];
+
         // eurtUsdtRoute = [
         //     { swapProtocol: 2, pool: eurtPoolAddress, fromCoin: weth.address, toCoin: usdt.address },
         //     { swapProtocol: 1, pool: fraxPoolAddress, fromCoin: usdt.address, toCoin: dai.address }
@@ -105,14 +123,17 @@ describe("Exchange (full setup operations)", async () => {
 
         const routes: Route[] = [
             eurtDaiRoute, eurtUsdcRoute, eurtUsdtRoute, daiEurtRoute, daiUsdcRoute, daiUsdtRoute,
-            usdcEurtRoute, usdcDaiRoute, usdcUsdtRoute, usdtEurtRoute, usdtDaiRoute, usdtUsdcRoute
+            usdcEurtRoute, usdcDaiRoute, usdcUsdtRoute, usdtEurtRoute, usdtDaiRoute, usdtUsdcRoute,
+            wMaticEurtRoute, wMaticDaiRoute, wMaticUsdcRoute, wMaticUsdtRoute
         ];
 
         await (await exchange.createInternalMajorRoutes(routes)).wait();
 
         const EurtAdapter = await ethers.getContractFactory("CurveEURtAdapter");
+        
 
         const eurtAdapter = await (await EurtAdapter.deploy()).deployed();
+        console.log("eurt Addres:", eurtAdapter.address)
 
         await (await exchange.registerAdapters([eurtAdapter.address], [2])).wait();
 
@@ -155,12 +176,13 @@ describe("Exchange (full setup operations)", async () => {
 
         signers = await ethers.getSigners();
 
+        wMatic = await ethers.getContractAt("IWrappedEther", "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270");
         weth = await ethers.getContractAt("IWrappedEther", "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619");
-        eurt = await ethers.getContractAt("IERC20Metadata", "0x172370d5Cd63279eFa6d502DAB29171933a610AF");
+        eurt = await ethers.getContractAt("IERC20Metadata", "0x7BDF330f423Ea880FF95fC41A280fD5eCFD3D09f");
         usdt = await ethers.getContractAt("IERC20Metadata", "0xc2132D05D31c914a87C6611C10748AEb04B58e8F");
         usdc = await ethers.getContractAt("IERC20Metadata", "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174");
         dai = await ethers.getContractAt("IERC20Metadata", "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063");
-        alluo = await ethers.getContractAt("IERC20Metadata", "0xb625dA6b38FD7c62747c777121140cA4b3D7fD05");
+        // alluo = await ethers.getContractAt("IERC20Metadata", "0xb625dA6b38FD7c62747c777121140cA4b3D7fD05");
         eurtLp = await ethers.getContractAt("IERC20Metadata", "0x600743B1d8A96438bD46836fD34977a00293f6Aa")
 
 
@@ -204,13 +226,13 @@ describe("Exchange (full setup operations)", async () => {
 
 
     it("Should check all available swaps", async () => {
-        const supportedCoinList = [dai, usdc, usdt, eurt, weth];
+        const supportedCoinList = [dai, usdc, usdt, eurt, wMatic];
         await weth.deposit({ value: parseEther("100.0") });
 
         // get all supported coins - swap ETH for all coins
         for (let i = 0; i < supportedCoinList.length; i++) {
             const coin = supportedCoinList[i];
-            if (coin.address == weth.address) continue;
+            if (coin.address == wMatic.address) continue;
             const ethToCoinAmount = parseEther("10.0");
             await testSwap(nativeEth, coin.address, ethToCoinAmount);
         }
@@ -236,3 +258,11 @@ describe("Exchange (full setup operations)", async () => {
         }
     });
 });
+
+// Is Swap protocol & Protocol ID same ? 
+// nested array at Create LP Token 
+// Replace Native / Wrapper Eth w Matic 
+
+
+// Adapter - Add liquidity, entry coin [4] - 1 ? 
+// 
